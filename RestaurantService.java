fe79ad991618df8183927b797f3c479c0c19bf52
Madhumitha-1404 <120001123+Madhumitha-1404@users.noreply.github.com>
2023-03-09 22:01:07 +0530

    Add files via upload

---
+++ RestaurantService.java
@@ -0,0 +1,16 @@
+package com.example.demo.Service;
+
+import java.util.List;
+
+import com.example.demo.model.SwiggyModel;
+
+/* here we declare some of the methods like save hotel details,get hotel details ,
+ * get hotel details by id, updating and delete details by id*/
+
+public interface RestaurantService {
+	SwiggyModel saveHotel(SwiggyModel hotel) ;
+	List<SwiggyModel>  getHotelFromDb() ;
+	SwiggyModel getHotelById(int hotelId) ;
+	SwiggyModel updateHotelDetails(SwiggyModel hotel, int hotelId) ;
+	void deleteHotelById(int hotelId) ;
+}
